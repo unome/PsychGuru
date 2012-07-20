003f48cbb3aceba2d924a4700a6c135ea365f87f
unome <agadde01@172.24.1.121>
2012-07-20 12:05:53 -0400

    first commit

---
+++ gen/com/apuroopgadde/psychguru/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.apuroopgadde.psychguru;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/com/apuroopgadde/psychguru/HomeActivity.java
@@ -0,0 +1,26 @@
+package com.apuroopgadde.psychguru;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.support.v4.app.NavUtils;
+
+public class HomeActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
+    }
+
+    
+}
