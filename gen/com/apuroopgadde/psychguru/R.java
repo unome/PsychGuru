b25c8b0cd2d1d5586f1c9df99b8719deea963429
unome <agadde01@172.24.1.121>
2012-07-21 14:43:53 -0400

    added code
    Signed-off-by: unome <agadde01@172.24.1.121>

--- gen/com/apuroopgadde/psychguru/R.java
+++ gen/com/apuroopgadde/psychguru/R.java
@@ -25,11 +25,11 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int button1=0x7f090006;
+        public static final int button_ok=0x7f090003;
         public static final int menu_settings=0x7f090007;
-        public static final int rB_diagCons=0x7f090004;
-        public static final int rB_management=0x7f090005;
-        public static final int rB_subTopic=0x7f090003;
+        public static final int rB_diagCons=0x7f090005;
+        public static final int rB_management=0x7f090006;
+        public static final int rB_subTopic=0x7f090004;
         public static final int sp_topics=0x7f090001;
         public static final int tV_subTopic=0x7f090002;
         public static final int textView1=0x7f090000;
--- src/com/apuroopgadde/psychguru/HomeActivity.java
+++ src/com/apuroopgadde/psychguru/HomeActivity.java
@@ -1,33 +1,92 @@
 package com.apuroopgadde.psychguru;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.app.Activity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.content.SharedPreferences;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.support.v4.app.NavUtils;
+import android.widget.Toast;
 
-public class HomeActivity extends Activity {
-
+public class HomeActivity extends Activity implements OnItemSelectedListener{
+	final String TAG="psychGuru";
+	ArrayList<String> excTopics = new ArrayList<String>();
+	RadioGroup rG_subTopic;
+	TextView tV_subTopic;
+	String selTopic=null;
+	String selSubTopic=null;
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.home);
+		excTopics.add("Psychopharmacology");
+		excTopics.add("Psychotherapy");
+		excTopics.add("Aids during clerkship");
+		excTopics.add("Click to select");
+		final SharedPreferences wmbPreference = PreferenceManager.getDefaultSharedPreferences(this);
+		boolean isFirstRun = wmbPreference.getBoolean("FIRSTRUN", true);
+		if (isFirstRun)
+		{
+			SharedPreferences.Editor editor = wmbPreference.edit();
+			editor.putBoolean("FIRSTRUN", false);
+			editor.putInt("currQuestion",1);
+			editor.putInt("currentTopic", -1);
+			editor.putInt("currentSubTopic",-1);
+			editor.putInt("currentScore", 0);
+			editor.commit();
+		}
 		showContent();
 
 	}
 
 	private void showContent() {
-		RadioGroup rB_subTopic = (RadioGroup)findViewById(R.id.rB_subTopic);
-		TextView tV_subTopic=(TextView)findViewById(R.id.tV_subTopic);
-		rB_subTopic.setVisibility(View.INVISIBLE);
+		
+		rG_subTopic = (RadioGroup)findViewById(R.id.rB_subTopic);
+		tV_subTopic=(TextView)findViewById(R.id.tV_subTopic);
+		rG_subTopic.setVisibility(View.INVISIBLE);
 		tV_subTopic.setVisibility(View.INVISIBLE);
 		Spinner sp_topics=(Spinner)findViewById(R.id.sp_topics);
-		
+		sp_topics.setOnItemSelectedListener(this);
+	}
+
+	@Override
+	public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+		selTopic = parent.getItemAtPosition(position).toString();
+		if(!excTopics.contains(selTopic))
+		{
+			tV_subTopic.setVisibility(View.VISIBLE);
+			rG_subTopic.setVisibility(View.VISIBLE);
+		}
+	}
+
+	public void onClickHandler(View v)
+	{
+		if(v.getId()==R.id.button_ok)
+		{
+			if(selTopic.equals("Click to select"))
+			{
+				Toast.makeText(v.getContext(), "Please select a topic",
+						Toast.LENGTH_LONG).show();
+			}
+			if(!excTopics.contains(selTopic)&&selSubTopic==null)
+			{
+				Toast.makeText(v.getContext(), "Please select a sub topic",
+						Toast.LENGTH_LONG).show();
+			}
+		}
+	}
+
+
+	@Override
+	public void onNothingSelected(AdapterView<?> arg0) {
+		// TODO Auto-generated method stub
+
 	}
 
 
